1c23f60bacc418c1c53d8465f0d678ab89b61cdc
salty-walty <waltonsj3@gmail.com>
2020-07-16 10:49:14 -0400

    homepage desktop built out

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -6,6 +6,12 @@
     <?php 
       // ACF vars for the page
       $btbt = get_field('below_the_banner_text');
+      $lch = get_field('left_column_header');
+      $lcc = get_field('left_column_content');
+      $rch = get_field('right_column_header');
+      $rcc = get_field('right_column_content');
+      $sch = get_field('symbols_section_header');
+      $scp = get_field('symbols_section_intro_paragraph');
     ?>
   	<div class="row">
   		<div class="col-md-12">	
@@ -71,13 +77,90 @@
   	</div>
   	<div class="row creation">
   		<div class="col-md-1"></div>
-  		<div class="col-md-10">
-  			<h2>
-  				<?php echo $btbt; ?>
-  			</h2>
-  		</div>
+      <div class="col-md-4">
+        <h3>
+          <?php echo $lch; ?>
+        </h3>
+        <div>
+          <?php echo $lcc; ?>
+        </div>
+      </div>
+      <div class="col-md-1"></div>
+      <div class="col-md-5">
+        <h3>
+          <?php echo $rch; ?>
+        </h3>
+        <div>
+          <?php echo $rcc; ?>
+        </div>
+      </div>
   		<div class="col-md-1"></div>
   	</div>
+
+    <div class="row symbols">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+        <div class="mother">
+          <div class="symbols-intro">        
+            <h3><?php echo $sch; ?></h3>
+            <p>
+              <?php echo $scp ?>
+            </p>
+          </div>
+        </div>
+          <?php if( have_rows('symbols_row') ): ?> 
+            <?php while( have_rows('symbols_row') ): the_row(); 
+              // vars
+              $lcs = get_sub_field('left_column_symbol');
+              $mcs = get_sub_field('middle_column_symbol');
+              $rcs = get_sub_field('right_column_symbol');
+            ?>
+            <div class="row symbols-row">
+              <div class="col-md-4">
+                <img src="<?php echo $lcs['url']; ?>" alt="<?php echo $lcs['alt']; ?>">
+                <h5>
+                  <?php echo $lcs['title']; ?>
+                </h5>
+                <p>
+                  <?php echo $lcs['caption']; ?>
+                </p>
+                <p>
+                  <?php echo $lcs['description']; ?>
+                </p>
+              </div>
+              <div class="col-md-4">
+                <img src="<?php echo $mcs['url']; ?>" alt="<?php echo $mcs['alt']; ?>">
+                <h5>
+                  <?php echo $mcs['title']; ?>
+                </h5>
+                <p>
+                  <?php echo $mcs['caption']; ?>
+                </p>
+                <p>
+                  <?php echo $mcs['description']; ?>
+                </p>
+              </div>
+              <div class="col-md-4">
+                <img src="<?php echo $rcs['url']; ?>" alt="<?php echo $rcs['alt']; ?>">
+                <h5>
+                  <?php echo $rcs['title']; ?>
+                </h5>
+                <p>
+                  <?php echo $rcs['caption']; ?>
+                </p>
+                <p>
+                  <?php echo $rcs['description']; ?>
+                </p>
+              </div>
+            </div>        
+            <?php 
+              endwhile; 
+            ?>
+           
+          <?php endif; ?>
+      </div>
+      <div class="col-md-1"></div>
+    </div>
   </section>
   <br>
   <br>
